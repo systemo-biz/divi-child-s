8fc58d1f9f3f2df4b34566e7d8200efde88e0ed2
Vitaliy Max <vm@casepress.org>
2016-03-21 18:45:03 +0500

    Первая версия

---
+++ footer.php
@@ -0,0 +1,60 @@
+<?php if ( 'on' == et_get_option( 'divi_back_to_top', 'false' ) ) : ?>
+
+	<span class="et_pb_scroll_top et-pb-icon"></span>
+
+<?php endif;
+
+if ( ! is_page_template( 'page-template-blank.php' ) ) : ?>
+
+			<footer id="main-footer">
+				<?php get_sidebar( 'footer' ); ?>
+
+
+		<?php
+			if ( has_nav_menu( 'footer-menu' ) ) : ?>
+
+				<div id="et-footer-nav">
+					<div class="container">
+						<?php
+							wp_nav_menu( array(
+								'theme_location' => 'footer-menu',
+								'depth'          => '1',
+								'menu_class'     => 'bottom-nav',
+								'container'      => '',
+								'fallback_cb'    => '',
+							) );
+						?>
+					</div>
+				</div> <!-- #et-footer-nav -->
+
+			<?php endif; ?>
+
+				<div id="footer-bottom">
+					<div class="container clearfix">
+						<div class="one_half">
+
+							<?php
+							// виджеты в баре
+							if ( function_exists('dynamic_sidebar') ) {
+								dynamic_sidebar('s-sidebar-1');
+							} ?>
+						</div>
+						<div class="one_half et_column_last">
+							<?php
+							if ( function_exists('dynamic_sidebar') ) {
+								dynamic_sidebar('s-sidebar-2');
+							}
+							?>
+						</div>
+					</div>	<!-- .container -->
+				</div>
+			</footer> <!-- #main-footer -->
+		</div> <!-- #et-main-area -->
+
+<?php endif; // ! is_page_template( 'page-template-blank.php' ) ?>
+
+	</div> <!-- #page-container -->
+
+	<?php wp_footer(); ?>
+</body>
+</html>
---
+++ functions.php
@@ -0,0 +1,34 @@
+<?php
+//Подключение стиля родительской темы
+add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
+function theme_enqueue_styles() {
+    wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+}
+function my_scripts_method() {
+	wp_enqueue_script('custom-script',
+		'http://malsup.github.com/jquery.form.js',
+		array('jquery')
+	);
+}
+add_action( 'wp_enqueue_scripts', 'my_scripts_method' );
+
+function s_widgets_init() {
+	register_sidebar( array(
+		'name' => 'Бар в футере колонка №1',
+		'id' => 's-sidebar-1',
+		'before_widget' => '<div id="%1$s" class="et_pb_widget %2$s">',
+		'after_widget' => '</div> <!-- end .et_pb_widget -->',
+		'before_title' => '<h4 class="widgettitle">',
+		'after_title' => '</h4>',
+	) );
+
+	register_sidebar( array(
+		'name' => 'Бар в футере колонка №2',
+		'id' => 's-sidebar-2',
+		'before_widget' => '<div id="%1$s" class="fwidget et_pb_widget %2$s">',
+		'after_widget' => '</div> <!-- end .fwidget -->',
+		'before_title' => '<h4 class="title">',
+		'after_title' => '</h4>',
+	) );
+}
+add_action( 'widgets_init', 's_widgets_init' );
